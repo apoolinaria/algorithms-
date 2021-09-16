e55c2758cedd7dd37ec3c98986e69cb704a9916c
Polina <chebanenkopolina@gmail.com>
2021-09-16 17:58:49 -0400

    integer reverse solved

--- integerReverse/integerReverse.js
+++ integerReverse/integerReverse.js
@@ -10,11 +10,19 @@
  */
 
 function reverseInteger(number) {
-  // set the result and helpers
+  let result = 0;
+  // smaller power of 10
+  let power = 10;
   // while the number is not 0
+  while (number > 0) {}
   // make room in result
+  result *= 10;
   // add the new digit
+  result += (number % power) / (power / 10);
   // remove the digit from number
-  // update place for the next iteartion
+  number -= number % power;
+  // update power for the next iteartion 10 at first and then 100 => 1000 => 10000 depending on how big is the initial number
+  power *= 10;
   // return result
+  return result;
 }
