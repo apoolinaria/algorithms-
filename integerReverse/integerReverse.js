fe952066a72d72459773bdcbedbc177daa044aed
Polina <chebanenkopolina@gmail.com>
2021-09-16 17:46:33 -0400

    integer reverse structure set

--- integerReverse/integerReverse.js
+++ integerReverse/integerReverse.js
@@ -1,14 +1,20 @@
-/* 
-* 
-* Integer Reverse
-* 
-* Given a positive integer, return its digits reversed. 
-* 
-* - DO NOT CONVERT THE INPUT INTO A STRING OR ARRAY. 
-* - Only use integers and math in your solution.
-*
-*/
+/*
+ *
+ * Integer Reverse
+ *
+ * Given a positive integer, return its digits reversed.
+ *
+ * - DO NOT CONVERT THE INPUT INTO A STRING OR ARRAY.
+ * - Only use integers and math in your solution.
+ *
+ */
 
-function reverseInteger(number){
-  // TODO: Implement this function!
+function reverseInteger(number) {
+  // set the result and helpers
+  // while the number is not 0
+  // make room in result
+  // add the new digit
+  // remove the digit from number
+  // update place for the next iteartion
+  // return result
 }
