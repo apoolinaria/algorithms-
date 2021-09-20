381c91ad3eee71e111d88c680c847a4e16481016
Polina <chebanenkopolina@gmail.com>
2021-09-20 15:02:53 -0400

    contains dublicates solved

--- containsDublicates/containsDublicates.js
+++ containsDublicates/containsDublicates.js
@@ -1,3 +1,13 @@
 // Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct.
 
-var containsDuplicate = function (nums) {};
+var containsDuplicate = function (nums) {
+  let hash = {};
+  for (let i = 0; i < nums.length; i++) {
+    if (hash[nums[i]] === undefined) {
+      hash[nums[i]] = 1;
+    } else if (hash[nums[i]] !== undefined) {
+      return true;
+    }
+  }
+  return false;
+};
