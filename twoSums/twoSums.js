b0cd5a6d5c057493fea4415ab1c600b0122c1957
Polina <chebanenkopolina@gmail.com>
2021-09-20 12:59:07 -0400

    twoSum solved

--- twoSums/twoSums.js
+++ twoSums/twoSums.js
@@ -6,4 +6,15 @@ You can return the answer in any order.
 *
 */
 
-var twoSum = function (nums, target) {};
+var twoSum = function (nums, target) {
+  let result = [];
+  for (let i = 0; i < nums.length; i++) {
+    for (let j = i + 1; j < nums.length; j++) {
+      if (nums[i] + nums[j] === target) {
+        result.push(i, j);
+        break;
+      }
+    }
+  }
+  return result;
+};
