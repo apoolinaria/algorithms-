44d3539959fa13596cbcd7df3c2c07e76f27d461
Polina <chebanenkopolina@gmail.com>
2021-09-24 18:18:07 -0400

    threeSum solved

--- threeSum/threeSum.js
+++ threeSum/threeSum.js
@@ -4,6 +4,25 @@ var threeSum = function (nums) {
 
   console.log('sorted', sorted, 'x', x);
   for (let i = 0; i < sorted.lenght; i++) {
+    if (i > 0 && sorted[i] == sorted[i - 1]) {
+      continue;
+    }
+    let left = i + 1;
+    let right = sorted.length - 1;
+    while (right > left) {
+      let sum = sorted[i] + sorted[left] + sorted[right];
+      if (sum > 0) {
+        right--;
+      } else if (sum < 0) {
+        left++;
+      } else {
+        result.push(sorted[i] + sorted[left] + sorted[right]);
+        left++;
+        while (sorted[left] === sorted[left + 1] && right > left) {
+          left++;
+        }
+      }
+    }
     /*
      check if i is not the first and if its not the same as the current value
        continue
@@ -21,6 +40,7 @@ var threeSum = function (nums) {
   }
   return result;
 };
+
 let nums = [-1, 0, 1, 2, -1, -4];
 // Output: [[-1,-1,2],[-1,0,1]]
 console.log(threeSum(nums));
