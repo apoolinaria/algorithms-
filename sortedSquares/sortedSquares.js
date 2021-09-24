d8332c0bd686fc3e70f34fb9408e9409c0cd78d4
Polina <chebanenkopolina@gmail.com>
2021-09-24 11:04:41 -0400

    sortedSquares beautifully solved

--- sortedSquares/sortedSquares.js
+++ sortedSquares/sortedSquares.js
@@ -7,4 +7,24 @@ Output: [0,1,9,16,100]
 Explanation: After squaring, the array becomes [16,1,0,9,100].
 After sorting, it becomes [0,1,9,16,100].
 */
-var sortedSquares = function (nums) {};
+var sortedSquares = function (nums) {
+  let result = [];
+  let right = nums.length - 1;
+  let left = 0;
+  let point = right;
+
+  while (left <= right) {
+    if (nums[left] ** 2 > nums[right] ** 2) {
+      result[point] = nums[left] ** 2;
+      point--;
+      left++;
+    } else {
+      result[point] = nums[right] ** 2;
+      point--;
+      right--;
+    }
+  }
+  return result;
+  // iterate over the array to compute squared array
+  // iterate over the array with two pointers sprting the array
+};
