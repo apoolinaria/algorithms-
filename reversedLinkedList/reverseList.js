7fafc4718f222d086485215c5683772c319f84fa
Polina <chebanenkopolina@gmail.com>
2021-10-04 17:04:46 -0400

    reversedList solved with reverse in place

--- reversedLinkedList/reverseList.js
+++ reversedLinkedList/reverseList.js
@@ -1,1 +1,12 @@
-var reverseList = function (head) {};
+var reverseList = function (head) {
+  let current = head;
+  let next = null;
+  let previous = null;
+  while (!head) {
+    next = current.next;
+    current.next = previous;
+    previous = current;
+    current = next;
+  }
+  return previous;
+};
