92b9d62bba36a48058f0b5d1b5c4d885b2669f0d
Polina <chebanenkopolina@gmail.com>
2021-10-04 17:01:53 -0400

    linkedListCycle solved

--- linsedListCycle1/linsedListCycle1.js
+++ linsedListCycle1/linsedListCycle1.js
@@ -1,1 +1,15 @@
-var hasCycle = function (head) {};
+var hasCycle = function (head) {
+  if (head === null) {
+    return false;
+  }
+  let slow = head;
+  let fast = head.next;
+  while (slow !== fast) {
+    if (!fast || !fast.next || !fast.next.next) {
+      return false;
+    }
+    slow = slow.next;
+    fast = fast.next.next;
+  }
+  return true;
+};
