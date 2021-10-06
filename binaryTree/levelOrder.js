d3a9da6bf564dc5db2eec4f4bf06d5e80aee184f
Polina <chebanenkopolina@gmail.com>
2021-10-06 16:52:54 -0400

    leverOrder solved

--- binaryTree/levelOrder.js
+++ binaryTree/levelOrder.js
@@ -1,2 +1,19 @@
 // Given the root of a binary tree, return the level order traversal of its nodes' values. (i.e., from left to right, level by level).
-var levelOrder = function () {};
+var levelOrder = function () {
+  var result = [];
+  var queue = [root];
+  while (root.length) {
+    var length = queue.length;
+    result.push(queue.map((node) => node.val));
+    while (length--) {
+      var node = queue.shift();
+      if (node.left) {
+        queue.push(node.left);
+      }
+      if (node.right) {
+        queue.push(node.right);
+      }
+    }
+  }
+  return result;
+};
